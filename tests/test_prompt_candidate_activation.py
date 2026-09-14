2896e0322dda4a34eb06c11c2e32222313034c8b
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-09-14 12:48:13 +0100

    Remove tests for retired candidate activation

--- tests/test_prompt_candidate_activation.py
+++
@@ -1,8 +0,0 @@
-def test_scheduler_installs_prompt_candidate_policy():
-    import app.institutional_two_zone as institutional_two_zone
-    import app.intraday_engine as intraday_engine
-    import app.prompt_candidate_policy as policy
-    import app.scheduler  # noqa: F401
-
-    assert intraday_engine.build_candidates is policy.build_prompt_candidates
-    assert institutional_two_zone.build_candidates is policy.build_prompt_candidates
